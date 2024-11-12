59a6b6987a3b27d840b083d88af9ad756e608924
Eero Avaro <eavaro@gmail.com>
2024-11-12 13:15:18 +0200

    Nav + footer päivitykset.

--- lpScript.js
+++ lpScript.js
@@ -1,14 +1,17 @@
-
 let modal = document.getElementById("modalBox");
-let btn = document.getElementById("modalBtn");
-let span = document.getElementsByClassName("close") [0];
-let closeBtn = document.getElementById("suljeIkkuna"); // Tää on se nappi alhaalla.  
+let buttons = document.querySelectorAll(".modalBtn"); // Valitsee kaikki modalBtn-classit.
 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+let span = document.getElementsByClassName("close")[0]; // Tää on se X-nappi ylhäällä.
+let closeBtn = document.getElementById("suljeIkkuna"); // Tää on se sulje-nappi alhaalla.
 
+// Onclick-toiminnot jokaiselle buttonille.
+buttons.forEach(button => {
+    button.onclick = function() {
+        modal.style.display = "block";
+    }
+});
 
+// Sulkemistoiminnot
 span.onclick = function() {
     modal.style.display = "none";
 }
---
+++ paskaNavScript.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".paska_hamburger");
+const navMenu = document.querySelector(".paska_nav_links_hamburger");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
