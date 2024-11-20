a85e1cbfb1c2385340b52a21fce38e8b8c03a3f1
Mariel <mariel.hynynen@gmail.com>
2024-11-21 01:14:08 +0200

    Delete mh_aboutUs/mh_aboutUs_WTF directory

--- mh_aboutUs/mh_aboutUs_WTF/eeronNavFooter/ryhma_project_2_punttis-etusivu/paskaNavScript.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".paska_hamburger");
-const navMenu = document.querySelector(".paska_nav_links_hamburger");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-});
