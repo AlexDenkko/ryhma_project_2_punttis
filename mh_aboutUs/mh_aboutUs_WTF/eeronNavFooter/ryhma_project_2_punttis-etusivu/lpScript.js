76702426f7bb071fe4ab042093fcbcbed613311f
Mariel <mariel.hynynen@gmail.com>
2024-11-20 16:09:28 +0200

    Add files via upload
    koko nav ja footer räjähtää

---
+++ mh_aboutUs/mh_aboutUs_WTF/eeronNavFooter/ryhma_project_2_punttis-etusivu/lpScript.js
@@ -0,0 +1,27 @@
+let modal = document.getElementById("modalBox");
+let buttons = document.querySelectorAll(".modalBtn"); // Valitsee kaikki modalBtn-classit.
+
+let span = document.getElementsByClassName("close")[0]; // Tää on se X-nappi ylhäällä.
+let closeBtn = document.getElementById("suljeIkkuna"); // Tää on se sulje-nappi alhaalla.
+
+// Onclick-toiminnot jokaiselle buttonille.
+buttons.forEach(button => {
+    button.onclick = function() {
+        modal.style.display = "block";
+    }
+});
+
+// Sulkemistoiminnot
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event){
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
