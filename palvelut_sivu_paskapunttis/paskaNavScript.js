fc8f7304a8f494249a0c1af195265342449b3175
AlexDenkko <alex-sviridenko@outlook.com>
2025-02-06 00:21:44 +0200

    adding a comments and organizing code for better readability

--- palvelut_sivu_paskapunttis/paskaNavScript.js
+++ palvelut_sivu_paskapunttis/paskaNavScript.js
@@ -1,7 +1,10 @@
+// Select the hamburger menu and navigation menu elements
 const hamburger = document.querySelector(".paska_hamburger");
 const navMenu = document.querySelector(".paska_nav_links_hamburger");
 
+// Add click event listener to the hamburger menu
 hamburger.addEventListener("click", () => {
+  // Toggle the 'active' class on both the hamburger menu and navigation menu
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
